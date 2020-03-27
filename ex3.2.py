fe5da988783ee08471e64dc676d6cfe2d54e99b9
Sam Hrncir <samhrncir@gmail.com>
2020-03-27 19:10:05 -0400

    First commit

---
+++ ex3.2.py
@@ -0,0 +1,33 @@
+# Will output to console a string.
+print("I will now count my chickens:")
+
+# String followed by the math solution
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Will output to console a string.
+print("Now I will count the eggs:")
+
+# Will print the solution to the math problem.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Will output only a string.
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+# Will output the solution to the result of the comparison condition.
+print(3 + 2 < 5 - 7)
+
+# Will output string followed by the math solution.
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Will output a string.
+print("Oh, that's why it's False.")
+
+# Will output a string.
+print("How about some more.")
+
+# Will output a string followed by the result of the comparison condition.
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3.py
@@ -0,0 +1,33 @@
+# Will output to console a string.
+print("I will now count my chickens:")
+
+# String followed by the math solution
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Will output to console a string.
+print("Now I will count the eggs:")
+
+# Will print the solution to the math problem.
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Will output only a string.
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Will output the solution to the result of the comparison condition.
+print(3 + 2 < 5 - 7)
+
+# Will output string followed by the math solution.
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Will output a string.
+print("Oh, that's why it's False.")
+
+# Will output a string.
+print("How about some more.")
+
+# Will output a string followed by the result of the comparison condition.
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
