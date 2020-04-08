e7c9b9e72428f39b30cc273b8cbfbcf119f588b7
Sam Hrncir <samhrncir@gmail.com>
2020-04-08 19:55:25 -0400

    Renamed files, standardized file naming

---
+++ ex01.0 A Good First Program.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("Marisa")
+# This is a comment
+print("this is the line after the comment")
