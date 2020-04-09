9aa1b78a75976d6d061fefe1fafb1a5091eab2c5
Sam Hrncir <samhrncir@gmail.com>
2020-04-08 20:05:37 -0400

    removed old named files, started excise 6

--- ex3.1.py
+++
@@ -1,9 +0,0 @@
-
-weeks = 12
-print("The number of week I have left with Marisa: ", weeks)
-
-# dpw means days per weeek
-dpw = 3
-print("The average number of days per week that I will see her: ", dpw)
-
-print("The total number of days that I have left with her: ", weeks * dpw)
